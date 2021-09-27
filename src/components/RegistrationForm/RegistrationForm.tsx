f49fedb26415701ee363bffac474ee146791a8ac
Lucas Souza <lucas.souza@itbam.org.br>
2021-09-27 17:57:20 -0400

    feat: cleaning project and add material ui

---
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,10 @@
+function RegistrationForm() {
+  return (
+    <form>
+      <label htmlFor="name">Nome</label>
+      <input type="text" name="name" id="name" />
+    </form>
+  );
+}
+
+export default RegistrationForm;
