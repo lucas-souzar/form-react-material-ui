73e04aea1e0c1cc7d1dd871e70f78ff0cdd7e2b0
Lucas Souza <lucas.souza@itbam.org.br>
2021-09-28 11:00:07 -0400

    feat: add form based on materia uil and typography config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import "./App.css";
+import { Container, Typography } from "@material-ui/core";
+import "@fontsource/roboto";
+
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Formulário de Cadastro</h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">
+        Formulário de Cadastro
+      </Typography>
+
       <RegistrationForm />
-    </>
+    </Container>
   );
 }
 
--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,8 +1,44 @@
+import { Button, FormControlLabel, Switch, TextField } from "@material-ui/core";
+
 function RegistrationForm() {
   return (
     <form>
-      <label htmlFor="name">Nome</label>
-      <input type="text" name="name" id="name" />
+      <TextField
+        id="name"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="lastName"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        placeholder="000.000.000-00"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promotions" color="primary" defaultChecked />}
+      />
+
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="news" color="primary" defaultChecked />}
+      />
+
+      <Button variant="contained" color="primary">
+        Cadastrar
+      </Button>
     </form>
   );
 }
