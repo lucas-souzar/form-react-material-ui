702f321a78204db05e04562ad55f4d8ba7f36da0
Lucas Souza <lucas.souza@itbam.org.br>
2021-09-29 17:37:40 -0400

    feat: improve forms navigation

--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import DeliveryDataForm from "./DeliveryDataForm";
 
 import PersonalDataForm from "./PersonalDataForm";
@@ -6,18 +7,46 @@ import UserDataForm from "./UserDataForm";
 
 const RegistrationForm: React.FC<any> = () => {
   const [actualStep, setActualStep] = useState(0);
+  const [collectedData, setCollectedData] = useState({});
+  useEffect(() => {
+    if (actualStep === forms.length - 1) onSubmitData(collectedData);
+  });
+
+  const collectData = (data: any) => {
+    setCollectedData({ ...collectedData, ...data });
+    next();
+  };
 
   const next = () => {
     setActualStep(actualStep + 1);
   };
 
   const forms = [
-    <UserDataForm onSave={next} />,
-    <PersonalDataForm onSave={next} validateCPF={validateCPF} />,
-    <DeliveryDataForm onSave={onSubmitData} />,
+    <UserDataForm onSave={collectData} />,
+    <PersonalDataForm onSave={collectData} validateCPF={validateCPF} />,
+    <DeliveryDataForm onSave={collectData} />,
+    <Typography variant="h5">Obrigado pelo cadastro!</Typography>,
   ];
 
-  return <>{forms[actualStep]}</>;
+  return (
+    <>
+      <Stepper activeStep={actualStep} style={{ margin: "1.5rem 0" }}>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Pessoal</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Finalização</StepLabel>
+        </Step>
+      </Stepper>
+      {forms[actualStep]}
+    </>
+  );
 };
 
 const onSubmitData = (data?: any) => {
