ac15d42a9261ee645414ab0c881d39741bd376b3
Lucas Souza <lucas.souza@itbam.org.br>
2021-09-29 11:51:17 -0400

    feat: added event in delivery form

--- src/components/RegistrationForm/DeliveryDataForm.tsx
+++ src/components/RegistrationForm/DeliveryDataForm.tsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField } from "@material-ui/core";
 
-interface DeliveryDataFormProps {
-  onSave: () => void;
+export interface Delivery {
+  cep: string;
+  address: string;
+  phoneNumber: string;
+  state: string;
+  city: string;
+}
+
+declare interface DeliveryDataFormProps {
+  onSave: (delivery: Delivery) => void;
 }
 
 const DeliveryDataForm: React.FC<DeliveryDataFormProps> = (props) => {
+  const [cep, setCEP] = useState("");
+  const [address, setAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        props.onSave();
+        props.onSave({ cep, address, phoneNumber, state, city });
       }}
     >
       <TextField
+        value={cep}
+        onChange={(event) => setCEP(event.target.value)}
         id="cep"
         label="CEP"
         type="number"
@@ -21,6 +37,8 @@ const DeliveryDataForm: React.FC<DeliveryDataFormProps> = (props) => {
         variant="outlined"
       />
       <TextField
+        value={address}
+        onChange={(event) => setAddress(event.target.value)}
         id="address"
         label="Endereço"
         type="text"
@@ -29,6 +47,8 @@ const DeliveryDataForm: React.FC<DeliveryDataFormProps> = (props) => {
         fullWidth
       />
       <TextField
+        value={phoneNumber}
+        onChange={(event) => setPhoneNumber(event.target.value)}
         id="phoneNumber"
         label="Número"
         type="number"
@@ -36,6 +56,8 @@ const DeliveryDataForm: React.FC<DeliveryDataFormProps> = (props) => {
         variant="outlined"
       />
       <TextField
+        value={state}
+        onChange={(event) => setState(event.target.value)}
         id="state"
         label="Estado"
         type="text"
@@ -43,6 +65,8 @@ const DeliveryDataForm: React.FC<DeliveryDataFormProps> = (props) => {
         variant="outlined"
       />
       <TextField
+        value={city}
+        onChange={(event) => setCity(event.target.value)}
         id="city"
         label="Cidade"
         type="text"
